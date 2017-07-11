ae0937eeeb7d7d8af8968230e38c4a98fd40ea5a
NaeSato <310.nae@gmail.com>
2017-07-11 15:50:59 +0900

    Fix redirecting to singin page

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,4 @@
 class MessagesController < ApplicationController
-  before_action :redirect_signin
   def index
-  end
-
-  private
-  def redirect_signin
-    redirect_to new_user_session_path unless user_signed_in?
   end
 end
