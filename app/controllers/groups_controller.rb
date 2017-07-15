caebf595f4ad2d5f2e71ca6872db3f00f17fc252
NaeSato <310.nae@gmail.com>
2017-07-15 17:24:10 +0900

    Update form for creating group.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,21 @@
 class GroupsController < ApplicationController
   def new
     @group = Group.new
+    @group.user_groups.build
   end
 
   def edit
   end
 
   def create
+    @group = Group.create(group_params)
   end
 
   def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, {user_ids: []})
   end
 end
