8ca14bb7b5cfa1c837b0e05d6ddb6940c7084816
NaeSato <310.nae@gmail.com>
2017-07-29 17:34:11 +0900

    Update for sending messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,15 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
     @message = @group.messages.build
   end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to group_messages_path
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image)
+  end
 end
