3e35a12134ab699a14754b6ddae425c339fa4ac7
NaeSato <310.nae@gmail.com>
2017-07-29 14:17:25 +0900

    Update the form of routes setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root  'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :edit, :create, :update] do
-    resources :messages, only: [:index]
+  resources :groups, except: [:destroy, :show] do
+    resources :messages, only: :index
   end
 end
