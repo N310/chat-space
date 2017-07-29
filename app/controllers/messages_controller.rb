0491dfdc9fcb19be3d80bd36d198e336f0b4385a
NaeSato <310.nae@gmail.com>
2017-07-29 20:46:00 +0900

    Update for saving user_id and group_id to messages table

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,6 +12,6 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:body, :image)
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
