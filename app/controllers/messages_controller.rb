d2fc3aa939935c8dad7fedca3b779f9521f1dd3d
NaeSato <310.nae@gmail.com>
2017-08-05 18:56:58 +0900

    Fix messages validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,16 @@
 class MessagesController < ApplicationController
   def index
-    @group = Group.find(params[:group_id])
+    create_group_message_instance
     @message = @group.messages.build
-    @messages = @group.messages
   end
 
   def create
+    create_group_message_instance
     @message = Message.new(message_params)
     if @message.save
       redirect_to group_messages_path
     else
       flash.now[:error] = @message.errors[:base][0]
-      @group = Group.find(params[:group_id])
       render :index
     end
   end
@@ -19,5 +18,10 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def create_group_message_instance
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
   end
 end
