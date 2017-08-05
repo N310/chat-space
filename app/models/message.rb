3fc003b6b52cbdbab7361b9f935d0c3fdad3d8ba
NaeSato <310.nae@gmail.com>
2017-08-05 19:47:24 +0900

    Fix message model to use 'blank?' method

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class MessagesValidator < ActiveModel::Validator
   def validate(record)
     body = record.send(options[:messages][0])
     image_url = record.send(options[:messages][1]).url
-    if body == "" && !image_url
+    if body.blank? && image_url.blank?
       record.errors[:base] << "メッセージを入力してください"
     end
   end
