9563d7cfba2f6f589bc7650e23e8c39bb269b271
NaeSato <310.nae@gmail.com>
2017-08-19 12:32:43 +0900

    Fix link path and make helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def get_newmessage(belong_group)
+    new_message = belong_group.messages.order('created_at DESC').first
+    if new_message
+      if new_message.body
+        return  new_message.body 
+      elsif new_message.image
+        return "画像が投稿されています"
+      end
+    else
+      return "まだメッセージはありません"
+    end
+  end
+
 end
