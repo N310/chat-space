aefaa544cee8312ceaf50fe870f5397c28d2d91c
NaeSato <310.nae@gmail.com>
2017-09-02 19:28:29 +0900

    Use Faker for dummy data

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
 
   factory :message do
-    body        "Hello"
+    body        Faker::Lorem.sentence
     image       Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'images', 'image.jpg'), 'image/jpg')
-    user_id     1
-    group_id    2
+    user_id     Faker::Number.between(1, 100)
+    group_id    Faker::Number.between(1, 100)
   end
 
 end
