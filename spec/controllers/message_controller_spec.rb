cae645e83574e4532ff80df3b6f0474c38bf2d8e
NaeSato <310.nae@gmail.com>
2017-09-09 15:38:40 +0900

    Add test that check instances of #index when user log in

---
+++ spec/controllers/message_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe MessagesController do
+  let (:user) { create(:user) }
+  let (:group) { create(:group) }
+  let (:messages) { create_list(:message, 2, user: user, group: group ) }
+
+  describe 'GET #index' do
+    context 'user login' do
+      before do
+        login_user user
+        get :index, params: { group_id: group }
+      end
+
+      it "assigns the requested group to @group" do
+        expect(assigns(:group)).to eq group
+      end
+
+      it "assigns the requested messages to @messages" do
+        expect(assigns(:messages)).to eq messages
+      end
+
+      it "assigns the requested message to @message" do
+        expect(assigns(:message)).to be_a_new Message
+      end
+    end
+  end
+end
