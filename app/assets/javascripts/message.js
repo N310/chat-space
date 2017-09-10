0580598edd4f13c27346dec775ef01b892a55544
NaeSato <310.nae@gmail.com>
2017-09-11 01:45:23 +0900

    Convert json data to html in message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,39 @@
 $(function () {
-  $("#new_message").submit(function(){
+  function buildHTML(message) {
+    messageImage = "";
+    if (message.image_url) {
+      messageImage = `<img src="${message.image_url}">`;
+    }
+    var html = `<div class="message">
+                  <p class="message__head">
+                    <span class="message__head_name">${message.user_name}</span>
+                    <span class="message__head_date">${message.created_at }</span>
+                  </p>
+                  <p class="message__body">
+                    ${message.body}
+                    ${messageImage}
+                  </p>
+                </div>`
+    return html;
+  }
+
+  $("#new_message").on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
+    var type = $(this).attr('method');
     $.ajax({
       url: url,
-      type: "POST",
+      type: type,
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false
+      contentType: false,
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.sendBox__input').val('');
     })
   })
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,4 @@
 json.user_name  @message.user.name
 json.created_at  @message.created_at
 json.body  @message.body
-json.image_url @message.image_url
+json.image_url @message.image_url(:thumb)
