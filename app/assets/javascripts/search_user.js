1bd39563ff609a84c3df318fdeca3aed78599aba
NaeSato <310.nae@gmail.com>
2017-09-16 15:44:40 +0900

    Add HTML for incremental search user

--- app/assets/javascripts/search_user.js
+++ app/assets/javascripts/search_user.js
@@ -1,4 +1,12 @@
 $(function(){
+  function appendHTML(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
+    $("#user-search-result").append(html);
+  }
+
   $(".chat-group-form__input").on("keyup", function() {
     var input = $(this).val();
 
@@ -9,8 +17,11 @@ $(function(){
       dataType: 'json'
     })
 
-    .done(function(user) {
-      console.log(user);
+    .done(function(users) {
+      $("#user-search-result").empty();
+      users.forEach(function(user){
+        appendHTML(user);
+      });
     });
   });
 });
