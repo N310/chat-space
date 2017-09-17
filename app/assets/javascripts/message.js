7e992cb3f9f6cfcadd4047be0d86358e1013145c
NaeSato <310.nae@gmail.com>
2017-09-17 12:53:20 +0900

    Automatic update of messages

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,7 +4,7 @@ $(function () {
     if (message.image_url) {
       messageImage = `<img src="${message.image_url}">`;
     }
-    var html = `<div class="message">
+    var html = `<div class="message" data-message-id="${message.id}">
                   <p class="message__head">
                     <span class="message__head_name">${message.user_name}</span>
                     <span class="message__head_date">${message.created_at }</span>
@@ -30,14 +30,45 @@ $(function () {
       contentType: false
     })
     .done(function(data){
-      var messages = $('.messages');
+      debugger;
+      var messagesArea = $('.messages');
       var html = buildHTML(data);
-      messages.append(html);
+      messagesArea.append(html);
       $('.sendBox__input').val('');
-      messages.animate({scrollTop: messages[0].scrollHeight}, 500, 'swing');
+      $(".sendBox__btn").prop("disabled", false);
+      messagesArea.animate({scrollTop: messagesArea[0].scrollHeight}, 500, 'swing');
     })
     .fail(function(){
       alert('error');
     })
   })
+
+  if ($('.messages').length) {
+    setInterval(function(){
+      var url = location.href;
+      var lastMessageId = $(".message:last").data("message-id");
+      $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json'
+      })
+      .done(function(data){
+        var messagesArea = $('.messages');
+        var length = data.length;
+        var latestMessageId = data[length-1].id;
+        if (latestMessageId > lastMessageId) {
+          var differenceNum = latestMessageId - lastMessageId;
+          for (i = differenceNum; i > 0; i--) {
+            var message = data[length-i]
+            var html = buildHTML(message);
+            messagesArea.append(html);
+          }
+        }
+        messagesArea.animate({scrollTop: messagesArea[0].scrollHeight}, 500, 'swing');
+      })
+      .fail(function(){
+        alert('メッセージが取得できません。');
+      }) 
+    },5000);
+  }
 })
